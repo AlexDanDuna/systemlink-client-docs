d6355d07390a0707c098e8c8f3f79a8357e09cc3
Alexandru Dan Duna <dunaalexdan@yahoo.com>
2026-06-26 14:13:45 +0300

    Remove further instances of CloudHttpConfiguration

--- examples/ExampleConfiguration.cs
+++ examples/ExampleConfiguration.cs
@@ -18,35 +18,20 @@ namespace NationalInstruments.SystemLink.Clients.Examples
         /// configuration. Exits if a suitable configuration is not available.
         /// </summary>
         /// <param name="args">The arguments used to run the example.</param>
-        /// <param name="allowCloud">Whether the example supports SystemLink
-        /// Cloud or not. When true (default), the --cloud argument may be used
-        /// to run the example with SystemLink Cloud. When false, the --cloud
-        /// argument will produce an error.</param>
         /// <returns>A configuration to use for the example.</returns>
-        public static IHttpConfiguration Obtain(
-            string[] args,
-            bool allowCloud = true)
+        public static IHttpConfiguration Obtain(string[] args)
         {
-            return new Parser(allowCloud).Parse(args);
+            return new Parser().Parse(args);
         }
 
         private class Parser
         {
-            private readonly bool _allowCloud;
-
-            public Parser(bool allowCloud)
-            {
-                _allowCloud = allowCloud;
-            }
-
             public IHttpConfiguration Parse(string[] args)
             {
                 if (args?.Length > 0)
                 {
                     switch (args[0])
                     {
-                        case "--cloud":
-                            return ObtainCloudOrExit(args);
                         case "--server":
                             return ObtainServerOrExit(args);
                         case "--help":
@@ -60,22 +45,6 @@ namespace NationalInstruments.SystemLink.Clients.Examples
                 {
                     return ObtainDefaultOrExit();
                 }
-            }
-
-            private IHttpConfiguration ObtainCloudOrExit(string[] args)
-            {
-                if (!_allowCloud)
-                {
-                    return PrintUsageAndExit("This example does not support SystemLink Cloud");
-                }
-
-                if (args.Length != 2)
-                {
-                    return PrintUsageAndExit(
-                        "--cloud requires 1 API key argument");
-                }
-
-                return new CloudHttpConfiguration(args[1]);
             }
 
             private IHttpConfiguration ObtainDefaultOrExit()
@@ -130,17 +99,8 @@ namespace NationalInstruments.SystemLink.Clients.Examples
                 Console.Error.WriteLine("managed by a SystemLink Server, do not specify any arguments. Otherwise,");
                 Console.Error.WriteLine("specify a configuration using one of the following arguments:");
                 Console.Error.WriteLine();
-                if (_allowCloud)
-                {
-                    Console.Error.WriteLine("\t--cloud <api_key>");
-                }
                 Console.Error.WriteLine("\t--server <url> [<username> <password>]");
                 Console.Error.WriteLine();
-                if (_allowCloud)
-                {
-                    Console.Error.WriteLine("Generate an API key on SystemLink Cloud. Go to https://www.systemlinkcloud.com,");
-                    Console.Error.WriteLine("log in with your NI User Account, and click Security to create an API key.");
-                }
                 Console.Error.WriteLine("To run the example against a SystemLink Server, the URL should include the");
                 Console.Error.WriteLine("scheme, host, and port if not default. For example:");
                 Console.Error.WriteLine("dotnet run -- --server https://myserver:9091 admin my_password");
